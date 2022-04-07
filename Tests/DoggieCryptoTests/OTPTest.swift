16f02b7b88805e8c4c50ac0597a819071e16fde4
Susan Cheng <susan.doggie@gmail.com>
2022-04-07 18:09:05 +0800

    Create OTPTest.swift

---
+++ Tests/DoggieCryptoTests/OTPTest.swift
@@ -0,0 +1,43 @@
+//
+//  OTPTest.swift
+//
+//  The MIT License
+//  Copyright (c) 2015 - 2022 Susan Cheng. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import DoggieCrypto
+import XCTest
+
+class OTPTest: XCTestCase {
+    
+    func testOTP() {
+        
+        var key = "secret_key"
+        let _key = key.withUTF8(SymmetricKey.init)
+        
+        let digest = HMAC<Insecure.SHA1>.authenticationCode(for: [0, 0, 0, 0, 0, 0, 0, 0], using: _key)
+        
+        XCTAssertEqual(OTP.generate(digest, .six), "051145")
+        XCTAssertEqual(OTP.generate(digest, .seven), "8051145")
+        XCTAssertEqual(OTP.generate(digest, .eight), "08051145")
+    }
+    
+}
